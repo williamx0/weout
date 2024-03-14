ce5b7db3e89796be30b2ce68982871de760d4d6b
William ☕️ <113069637+williamx0@users.noreply.github.com>
2024-03-14 17:36:56 +0100

    corrected the error in the AppUserRepository

---
+++ src/main/java/com/weout/appuser/AppUserRepository.java
@@ -0,0 +1,13 @@
+package com.weout.appuser;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+
+    Optional<AppUser> findByEmail(String email);
+}
